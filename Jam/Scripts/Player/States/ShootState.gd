76fd7ec24c015133bec08d0c870a050c8ffed149
zitaoyu <zitaoyu.work@gmail.com>
2024-03-11 19:22:34 -0700

    player and basic enemy sprite integration

--- Jam/Scripts/Player/States/ShootState.gd
+++ Jam/Scripts/Player/States/ShootState.gd
@@ -5,15 +5,17 @@ extends State
 @export var projectile : PackedScene
 @export var shootpos : Node2D
 
-func enter():
+func enter():	
+	player.velocity = Vector2.ZERO
 	anim.play("shoot")
-	$Timer.start()
+	$ShootDelayTimer.start()
+	await anim.animation_finished
+	transition.emit(self, "idle")
+
+func _on_shoot_delay_timer_timeout():
 	var s : Area2D = projectile.instantiate()
-	get_parent().get_parent().get_parent().add_child(s)
 	s.position = shootpos.global_position
 	s.rotation = shootpos.global_rotation
+	get_parent().get_parent().get_parent().add_child(s)
 	s.set_meta("creator", owner)
-	player.velocity = Vector2.ZERO
-	
-func _on_timer_timeout():
-	transition.emit(self, "idle")
+
--- Jam/Scripts/Player/States/SlashState.gd
+++ Jam/Scripts/Player/States/SlashState.gd
@@ -5,15 +5,24 @@ extends State
 @export var slash : PackedScene
 @export var slashpos : Node2D
 
+var is_right_arm = true
+
 func enter():
+	if !is_right_arm:
+		anim.flip_h = true
+	player.velocity = Vector2.ZERO
 	anim.play("slash")
-	$Timer.start()
+	$AttackDelayTimer.start()
+	await anim.animation_finished
+	transition.emit(self, "idle")
+
+func exit():
+	anim.flip_h = false
+	is_right_arm = !is_right_arm
+
+func _on_attack_delay_timer_timeout():
 	var s : Area2D = slash.instantiate()
 	get_parent().get_parent().get_parent().add_child(s)
 	s.position = slashpos.global_position
 	s.rotation = slashpos.global_rotation
 	s.set_meta("creator", owner)
-	player.velocity = Vector2.ZERO
-	
-func _on_timer_timeout():
-	transition.emit(self, "idle")
