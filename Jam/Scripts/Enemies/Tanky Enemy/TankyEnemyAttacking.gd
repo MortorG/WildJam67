fb823e7fe5392b2054a654c78aeca2f1605d6225
Tommy Yu <96801705+zitaoyu@users.noreply.github.com>
2024-03-12 02:21:28 -0700

    Tanky enemy sprites (#4)

--- Jam/Scripts/Enemies/Tanky Enemy/TankyEnemyAttacking.gd
+++ Jam/Scripts/Enemies/Tanky Enemy/TankyEnemyAttacking.gd
@@ -4,14 +4,16 @@ extends State
 @export var attack_hitbox : CollisionShape2D
 
 func enter():
-	anim.play("attack")
+	anim.play("buildup")
 	$Buildup.start()
 
 func _on_buildup_timeout():
+	anim.play("attack")
 	attack_hitbox.set_deferred("disabled", false)
 	$AttackDuration.start()
 
 func _on_attack_duration_timeout():
+	anim.play("endattack")
 	attack_hitbox.set_deferred("disabled", true)
 	$Endlag.start()
 
