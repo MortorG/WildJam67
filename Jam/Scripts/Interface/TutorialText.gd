ba10507c8e53c264335841aefad41cbad4b69570
VaJau2 <vadim206-1998@yandex.ru>
2024-03-14 22:26:56 +0300

    tutorial skip by key

--- Jam/Scripts/Interface/TutorialText.gd
+++ Jam/Scripts/Interface/TutorialText.gd
@@ -1,14 +1,18 @@
 extends Label
 
 
-@onready
-var timer = $Timer
+@onready var timer = $Timer
+@onready var next_label = $Label
 
 
 signal spawn_enemy
+signal next_phrase
 
 
 func _ready():
+	await timer.timeout
+	next_label.visible = true
+	
 	var tutorial_data = G.read_json("tutorial_text")
 	for key in tutorial_data.keys():
 		var tutorial_item = tutorial_data[key]
@@ -17,6 +21,10 @@ func _ready():
 		if tutorial_item.has("spawn"):
 			spawn_enemy.emit()
 		
-		timer.start(tutorial_item.time)
-		await timer.timeout
+		await self.next_phrase
 	visible = false
+
+
+func _process(_delta) -> void:
+	if visible and Input.is_action_just_pressed("space"):
+		next_phrase.emit()
