086b8e3c10ae3556cd3db8a5929e8c1ead6b942a
Hugo Fuster Sanahuja <hugo310102@gmail.com>
2024-03-15 13:20:57 +0100

    fixed projetiles not colliding with walls

--- Jam/Scripts/KnockbackComponent.gd
+++ Jam/Scripts/KnockbackComponent.gd
@@ -1,9 +1,11 @@
 extends Node
 class_name KnockbackComponent
 
+@export var knockback_effect : float = 1
+
 func knock_back(dir, force):
 	var body := owner as PhysicsBody2D
-	body.velocity = dir * force
+	body.velocity = dir * force * knockback_effect
 	body.set_collision_mask_value(1, false)
 	body.set_collision_mask_value(2, false)
 	body.set_collision_layer_value(1, false)
